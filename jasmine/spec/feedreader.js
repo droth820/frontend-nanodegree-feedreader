78f580c071236f528755a919e6b4d1f2383f7dc6
David <davidroth0820@gmail.com>
2015-05-08 17:52:17 -0700

    added single quotes around class on line 77

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -70,10 +70,10 @@ $(function() {
         * clicked and does it hide when clicked again.
         */
         it('Menu shows when clicked then hidden when clicked a second time', function() {
-            $('menu-icon-link').click();
+            $('.menu-icon-link').onClick();
             expect($('body').hasClass('menu-hidden')).not.toBe(true);
             //expectation to hide on next click
-            $('.menu-icon-link').click();
+            $('.menu-icon-link').onClick();
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
